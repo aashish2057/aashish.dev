f698ab02215c8d947d821ed3f39115e395ae21e1
aashish2057 <aashish2057@gmail.com>
2023-10-28 15:44:33 -0500

    personal website done lmfao

---
+++ src/app/light/page.tsx
@@ -0,0 +1,10 @@
+export default function Light() {
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen font-semibold">
+        Did you really thing I would make a dark/light mode toggle? Why are you
+        still here? Do you have nothing better to do?
+      </div>
+    </>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen font-semibold">
+        Do you think I have built anything good enough to put here?
+      </div>
+    </>
+  );
+}
